4dd22bf44cd27f309fbb278a98ba06d7e67b1bc6
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-05-17 19:33:42 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 # CONNECT DB
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-from app.config import settings
+from config import settings
 
 engine = create_async_engine(settings.DATABASE_URL)
 
