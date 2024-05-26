c1afc039825f4a5c05301679e62481cd5a841415
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-05-27 00:46:49 +0500

    Update service.py

--- app/clothes/service.py
+++ app/clothes/service.py
@@ -1,7 +1,7 @@
 from app.service.base import BaseService
 from app.clothes.models import Clothes, clothes_weatherLabels_association
 from app.database import async_session_maker
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from app.weathers.models import Weathers
 from app.weather_labels.models import Weather_labels
 
@@ -12,15 +12,14 @@ class ClothesService(BaseService):
     async def get_clothes_for_weather(cls, weather_id: int, feels_like: int, month: int):
         async with async_session_maker() as session:
             query = (
-                select(cls.model)
-                .join(clothes_weatherLabels_association)
-                .join(Weathers)
-                .join(Weather_labels)
-                .filter(clothes_weatherLabels_association.c.weather_labels_id == weather_id)
-                .filter(clothes_weatherLabels_association.c.weathers_id == (int(month % 12 / 3) + 1))
-                .filter(Clothes.temp_min <= feels_like)
-                .filter(Clothes.temp_max >= feels_like)
-            )
+				select(Clothes, Weathers.name.label('season'), WeatherLabels.name.label('weather_label'))
+				.join(clothes_weatherLabels_association)
+				.join(Weathers)
+				.join(WeatherLabels)
+				.filter(Weathers.id == (int(month%12/3) + 1))
+				.filter(WeatherLabels.id == weather_id)
+				.filter(and_(Clothes.temp_min <= feels_like, Clothes.temp_max >= feels_like))
+			)
 
             result = await session.execute(query)
             return result.mappings().all()
