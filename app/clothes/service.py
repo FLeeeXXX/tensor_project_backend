61cc55c293b63cd22b0be3fdb77c603606d88fdc
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-05-27 00:14:42 +0500

    fix

--- app/clothes/service.py
+++ app/clothes/service.py
@@ -7,21 +7,20 @@ from app.weather_labels.models import Weather_labels
 
 class ClothesService(BaseService):
     model = Clothes
-	
-	
-	@classmethod
-	async def get_clothes_for_weather(cls, weather_id: int, feels_like: int, month: int):
-		async with async_session_maker() as session:
-			query = (
+
+    @classmethod
+    async def get_clothes_for_weather(cls, weather_id: int, feels_like: int, month: int):
+        async with async_session_maker() as session:
+            query = (
                 select(cls.model)
                 .join(clothes_weatherLabels_association)
-				.join(Weathers)
-				.join(Weather_labels)
+                .join(Weathers)
+                .join(Weather_labels)
                 .filter(clothes_weatherLabels_association.weather_labels_id == weather_id)
-				.filter(clothes_weatherLabels_association.weathers_id == (int(month%12/3) + 1))
+                .filter(clothes_weatherLabels_association.weathers_id == (int(month % 12 / 3) + 1))
                 .filter(Clothes.temp_min <= feels_like)
                 .filter(Clothes.temp_max >= feels_like)
             )
 
-			result = await session.execute(query)
+            result = await session.execute(query)
             return result.mappings().all()
