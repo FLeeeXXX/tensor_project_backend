7c64e155772673f11278b6e6c3109b64ba1016b0
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-05-29 02:53:29 +0500

    fix logout

--- app/users/router.py
+++ app/users/router.py
@@ -4,7 +4,7 @@ from app.users.schemas import SUsersRegister, SUsersAuth, SUsersRead
 from app.users.service import UsersService
 from app.users.models import Users
 from app.users.auth import get_password_hash, authenticate_user, create_access_token
-from app.users.dependencies import get_current_user
+from app.users.dependencies import get_current_user, get_token
 
 
 router = APIRouter(
@@ -12,7 +12,7 @@ router = APIRouter(
     tags=["Auth"]
 )
 
-
+# Добавить проверки на поля в регистрации
 @router.post("/register")
 async def register_user(user_data: SUsersRegister) -> None:
     existing_user = await UsersService.find_one_or_none(email=user_data.email)
@@ -21,7 +21,7 @@ async def register_user(user_data: SUsersRegister) -> None:
     hashed_password = get_password_hash(user_data.password)
     await UsersService.add(login=user_data.login, email=user_data.email, password=hashed_password)
 
-
+# Добавить проверки на поля в авторизации
 @router.post("/login")
 async def login_user(response: Response, user_data: SUsersAuth) -> SUsersRead:
     user = await authenticate_user(user_data.email, user_data.password)
@@ -29,10 +29,11 @@ async def login_user(response: Response, user_data: SUsersAuth) -> SUsersRead:
     response.set_cookie("weather_access_token", access_token, secure=True, httponly=True, samesite='none', expires=expire.strftime("%a, %d %b %Y %H:%M:%S GMT"))
     return SUsersRead.from_orm(user.Users)
 
-
+# Может подправить логику
 @router.post("/logout")
 async def logout_user(responce: Response) -> None:
-    responce.delete_cookie("weather_access_token")
+    if get_token(responce):
+        responce.delete_cookie("weather_access_token")
 
 
 @router.get("/me")
